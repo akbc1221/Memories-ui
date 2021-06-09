906bbaef2f05c9ca30f06573b84840c22a0289b1
Arnab Chattopadhyay <59999832+akbc1221@users.noreply.github.com>
2021-06-10 00:09:40 +0530

    auth done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Container, Grid, Grow } from "@material-ui/core";
+
+import Form from "../Form/Form.js";
+import Posts from "../Posts/Posts.js";
+import { getPosts } from "../../actions/posts.js";
+
+const Home = () => {
+  const [currentId, setCurrentId] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Grow in>
+      <Container>
+        <Grid
+          container
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
